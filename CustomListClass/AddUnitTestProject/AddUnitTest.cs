44c842e32df682e331c7a4928e5fc4e87ad7ceb4
unknown <bridgetott@yahoo.com>
2020-09-30 09:22:33 -0500

    fixed the Add_FivePositiveNumbers_CapacityEight test Method

--- CustomListClass/AddUnitTestProject/AddUnitTest.cs
+++ CustomListClass/AddUnitTestProject/AddUnitTest.cs
@@ -8,94 +8,59 @@ namespace AddUnitTestProject
     public class AddUnitTest
     {
         [TestMethod]
-        public void Add_ThreePositiveValues_CountThree()
+        public void Add_FivePositiveNumbers_CountFive()
         {
             //arrange
-            MyList<int> myList = new MyList<int>();
+            BridgetsList<int> bridgetsList = new BridgetsList<int>();
             int value1 = 1;
             int value2 = 2;
             int value3 = 3;
-            int expected = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int expected = 5;
             int actual;
             //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
+            bridgetsList.Add(value1);
+            bridgetsList.Add(value2);
+            bridgetsList.Add(value3);
+            bridgetsList.Add(value4);
+            bridgetsList.Add(value5);
+            actual = bridgetsList.count;
             //assert
             Assert.AreEqual(expected, actual);
         }
 
-        public void Add_ThreePositiveValues_CountThree()
+        [TestMethod]
+        public void Add_ThreePositiveNumbers_CapacityNine()
         {
             //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
+            BridgetsList<int> bridgetsList = new BridgetsList<int>();
+
+            int expected =
             int actual;
             //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
+
             //assert
             Assert.AreEqual(expected, actual);
         }
 
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
+        [TestMethod]
+        //public void Add_FivePositiveNumbers_CountFive()
+        //{
+        //    //arrange
+        //    BridgetsList<int> bridgetsList = new BridgetsList<int>();
+        //    int value1 = 1;
+        //    int value2 = 2;
+        //    int value3 = 3;
+        //    int expected = 3;
+        //    int actual;
+        //    //act
+        //    bridgetsList.Add(value1);
+        //    bridgetsList.Add(value2);git
+        //    bridgetsList.Add(value3);
+        //    actual = bridgetsList.count;
+        //    //assert
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
--- CustomListClass/AddUnitTestProject/RemoveUnitTest.cs
+++ CustomListClass/AddUnitTestProject/RemoveUnitTest.cs
@@ -8,94 +8,17 @@ namespace AddUnitTestProject
     public class RemoveUnitTest
     {
         [TestMethod]
-        public void Add_ThreePositiveValues_CountThree()
+        
+        public void Remove_TwoPositiveNumbers_CountisRemainingNumbers()
         {
             //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
+
             //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
+
             //assert
             Assert.AreEqual(expected, actual);
+
         }
 
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        public void Add_ThreePositiveValues_CountThree()
-        {
-            //arrange
-            MyList<int> myList = new MyList<int>();
-            int value1 = 1;
-            int value2 = 2;
-            int value3 = 3;
-            int expected = 3;
-            int actual;
-            //act
-            myList.Add(value1);
-            myList.Add(value2);
-            myList.Add(value3);
-            actual = myList.count;
-            //assert
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
